48534c4992288119af356b900ccdc1e6e9e9c458
Goh Wei Wen <goweiwen@gmail.com>
2017-01-21 17:49:33 +0800

    Added ejs and ejs views

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
-var koa = require('koa')
-var app = koa()
+'use strict'
 
-app.use(require('koa-static')('static', {}))
+const koa = require('koa')
+const logger = require('koa-logger')
+const Router = require('koa-router')
+const render = require('koa-ejs')
+const server = require('koa-static')
 
-app.use(function*() {
+const app = koa()
+
+app.use(logger())
+app.use(server(`${__dirname}/static`))
+
+render(app, {
+    root: `${__dirname}/view`,
+    layout: 'index',
+    viewExt: 'html',
+    debug: true,
+    cache: false
 })
 
-app.listen(3000)
+app.use(function*(next) {
+    // Use PDF.js like this:
+    // http://localhost:3000/web/viewer.html?file=http://localhost:3000/pdf/CS2100.pdf
+
+    yield this.render('index', {
+        body: 'hi'
+    })
+})
+
+const PORT = 3000
+app.listen(PORT)
+console.log(`listening on port ${PORT}`)
