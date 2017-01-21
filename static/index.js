279fcba65d3b30282240d40897ea31a1e1bfe6a7
Goh Wei Wen <goweiwen@gmail.com>
2017-01-22 04:10:13 +0800

    Swapped rendering order of PDF and chatbox

--- static/index.js
+++ static/index.js
@@ -7,8 +7,9 @@ class PDFDoc {
 
         this.canvas = document.createElement('canvas')
         this.canvas.style.position = 'absolute'
-        document.body.insertBefore(this.canvas, document.body.firstChild)
-        // document.body.appendChild(this.canvas)
+        this.canvas.style.zIndex = '-999'
+        // document.body.insertBefore(this.canvas, document.body.firstChild)
+        document.body.appendChild(this.canvas)
         this.context = this.canvas.getContext('2d')
 
         this.pdfDoc = null
