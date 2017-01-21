5e60a8950e1e8a65ce5cb258873f4446a61f3f4e
nixxholas <nixholas@outlook.com>
2017-01-21 17:01:23 +0800

    user.js: User object basic implementation
    Signed-off-by: nixxholas <nixholas@outlook.com>

---
+++ user.js
@@ -0,0 +1,10 @@
+function User(name) {
+  // this.id = // Something has to be done here to retrieve the incremental value of the id from the DB
+  this.name_ = name;
+}
+
+function getName() {
+  return this.name_;
+}
+
+
